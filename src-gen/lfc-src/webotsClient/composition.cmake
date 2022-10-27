c2e1b89c3e703b62e7c617f683dc2d8b29f16f5e
jackykwok2024 <jackykwok@berkeley.edu>
2022-10-27 01:03:29 -0700

    update linking

--- src-gen/lfc-src/webotsClient/composition.cmake
+++ src-gen/lfc-src/webotsClient/composition.cmake
@@ -1,7 +1,3 @@
 project(webotsClient)
 target_include_directories(webotsClient PUBLIC "$ENV{WEBOTS_HOME}/include/controller/c")
 target_link_libraries(webotsClient /lib/libController.so)
-
-#set (LIBRARIES ${CMAKE_SHARED_LIBRARY_PREFIX}Controller${CMAKE_SHARED_LIBRARY_SUFFIX} ${CMAKE_SHARED_LIBRARY_PREFIX}CppController${CMAKE_SHARED_LIBRARY_SUFFIX})
-#include_directories($ENV{WEBOTS_HOME}/include/controller/c $ENV{WEBOTS_HOME}/include/controller/cpp)
-#target_link_libraries(webotsClient ${LIBRARIES})
